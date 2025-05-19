4215a53d2cbf2bca68eddcc5c41b0e14461986d9
Sarthak Deshmukh <deshmukh.sar@northeastern.edu>
2025-05-18 23:18:21 -0400

    Added cors policy

--- server/server.js
+++ server/server.js
@@ -20,7 +20,12 @@ const mainTex = fs.readFileSync(mainTexPath, 'utf8');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
